73dc48a6a3c7a58497f017f18392d07665e387f7
Yosh <62856297+yoshakami@users.noreply.github.com>
2024-10-04 17:56:00 +0200

    add background autoLaunch

---
+++ WaccaStartupBackgroundLauncher.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static string WaccaCircleStartup = Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "WaccaCircleStartup.exe");
+    static void Main()
+    {
+        while (true)
+        {
+            Thread.Sleep(20000); // Sleep for 20 seconds (20,000 milliseconds)
+            try
+            {
+                // Create a new process to run WMIC command
+                Process process = new Process();
+                process.StartInfo.FileName = "wmic";
+                process.StartInfo.Arguments = "process where \"name like '%WaccaCircle%'\" get name, processid";
+                process.StartInfo.RedirectStandardOutput = true; // Capture the output
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                // Start the process
+                process.Start();
+
+                // Read and display the output
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                if (output == "\r\r\n\r\r\n")
+                {
+
+                    // Create a new process to run WMIC command
+                    Process process2 = new Process();
+                    process2.StartInfo.FileName = "wmic";
+                    process2.StartInfo.Arguments = "process where \"name like '%Mercury%'\" get name, processid";
+                    process2.StartInfo.RedirectStandardOutput = true; // Capture the output
+                    process2.StartInfo.UseShellExecute = false;
+                    process2.StartInfo.CreateNoWindow = true;
+
+                    // Start the process
+                    process2.Start();
+
+                    // Read and display the output
+                    output = process2.StandardOutput.ReadToEnd();
+                    process2.WaitForExit();
+                    if (output == "\r\r\n\r\r\n")
+                    {
+                        Process.Start(WaccaCircleStartup);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}

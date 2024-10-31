5e711aeed136bc7e01b5df593d1bf369ad7671a4
Yosh <62856297+yoshakami@users.noreply.github.com>
2024-10-31 14:13:36 +0100

    move 32 icons!

--- Mouse/WaccaCircleDesktop32.cs
+++ Mouse/WaccaCircleDesktop32.cs
@@ -40,8 +40,8 @@ class Program
     {
         Click(new Point(1079, 1919));
         Thread.Sleep(1000);
-        int[] hours_x = { 540, 770, 938, 1000, 938, 770, 540, 310, 141, 80, 141, 309, 540, 700, 817, 860, 817, 700, 540, 380, 262, 220, 262, 379 };
-        int[] hours_y = { 447, 508, 677, 907, 1137, 1305, 1367, 1305, 1137, 907, 677, 508, 587, 629, 747, 907, 1067, 1184, 1227, 1184, 1067, 907, 747, 630 };
+        int[] hours_x = { 540, 770, 938, 1000, 938, 770, 540, 310, 141, 80, 141, 309, 540, 700, 817, 860, 817, 700, 540, 380, 262, 220, 262, 379, 540, 697, 762, 697, 540, 382, 317, 382 };
+        int[] hours_y = { 447, 508, 677, 907, 1137, 1305, 1367, 1305, 1137, 907, 677, 508, 587, 629, 747, 907, 1067, 1184, 1227, 1184, 1067, 907, 747, 629, 684, 749, 907, 1064, 1129, 1064, 907, 382 };
         int x;
         int y;
         x = 150;
@@ -50,20 +50,21 @@ class Program
         int[] moved_y = { 50, 50, 50, 50 };
         Point startPos;
         Point endPos;
+        int y_space = 149;
+        int LAG_DELAY = 100;
         for (int k = 0; k < 4; k++)
         {
             Point startPos2 = new Point(x, y);
             Point endPos2 = new Point(moved_x[k], moved_y[k]);
             // Perform a drag-click
             DragClick(startPos2, endPos2);
-            Thread.Sleep(500);
+            Thread.Sleep(LAG_DELAY);
             y += 150;
         }
         x = 50;
         y = 200;
         int prev_x = 0;
         int prev_y = 0;
-        int y_space = 149;
         for (int j = 0; j < 2; j++)
         {
             for (int i = 1; i < 11; i++)
@@ -76,7 +77,7 @@ class Program
                         Point endPos3 = new Point(hours_x[k + 16], hours_y[k + 16]);
                         // Perform a drag-click
                         DragClick(startPos3, endPos3);
-                        Thread.Sleep(500);
+                        Thread.Sleep(LAG_DELAY);
                     }
                 }
                 if (j * 11 + i == 12)
@@ -85,12 +86,12 @@ class Program
                     endPos = new Point(hours_x[11], hours_y[11]);
                     // Perform a drag-click
                     DragClick(startPos, endPos);
-                    Thread.Sleep(500);
+                    Thread.Sleep(LAG_DELAY);
                     startPos = new Point(x, y);
                     endPos = new Point(hours_x[0], hours_y[0]);
                     // Perform a drag-click
                     DragClick(startPos, endPos);
-                    Thread.Sleep(500);
+                    Thread.Sleep(LAG_DELAY);
                     y += y_space;
                     continue;
                 }
@@ -103,7 +104,7 @@ class Program
                 Point endPos1 = new Point(hours_x[j * 11 + i], hours_y[j * 11 + i]);
                 // Perform a drag-click
                 DragClick(startPos1, endPos1);
-                Thread.Sleep(500);
+                Thread.Sleep(LAG_DELAY);
                 y += y_space;
             }
             prev_x = x;
@@ -113,19 +114,35 @@ class Program
         }
         startPos = new Point(prev_x, prev_y);
         endPos = new Point(hours_x[22], hours_y[22]);
-        // place the last circle
+        // place circle
         DragClick(startPos, endPos);
-        Thread.Sleep(500);
+        Thread.Sleep(LAG_DELAY);
         startPos = new Point(prev_x, prev_y + y_space);
         endPos = new Point(hours_x[23], hours_y[23]);
-        // place the last circle
+        // place circle
         DragClick(startPos, endPos);
-        Thread.Sleep(500);
+        Thread.Sleep(LAG_DELAY);
+        x = 260;
+        y = 50 + y_space;
         startPos = new Point(260, 50);
         endPos = new Point(hours_x[12], hours_y[12]);
-        // place the last circle
+        // place circle
         DragClick(startPos, endPos);
-        endPos = new Point(hours_x[12], hours_y[12] + 200);
+        Thread.Sleep(LAG_DELAY);
+        for (int i = 25; i < 33; i++)
+        {
+            startPos = new Point(x, y);
+            endPos = new Point(hours_x[i], hours_y[i]);
+            DragClick(startPos, endPos);
+            y += y_space;
+        }
+        startPos = new Point(x, y);
+        endPos = new Point(hours_x[24], hours_y[24]);
+        // place circle
+        DragClick(startPos, endPos);
+        Thread.Sleep(LAG_DELAY);
+        // deselect- click in the middle of nowhere
+        endPos = new Point(hours_x[24], hours_y[24] + 200);
         Click(endPos);
         // Give the user a moment to focus on the target application
         //Console.WriteLine("You have 5 seconds to focus on the target application...");
@@ -171,7 +188,7 @@ class Program
         SendInput(1, ref mouseInput, Marshal.SizeOf(mouseInput));
 
         // Simulate mouse movement
-        MoveMouseSmoothly(start, end, 500, 50);  // Move smoothly over 500 ms with 50 steps
+        MoveMouseSmoothly(start, end, 200, 30);  // Move smoothly over 200 ms with 30 steps
 
         // Simulate mouse up (left button release)
         mouseInput.mi.dwFlags = MOUSEEVENTF_LEFTUP;
